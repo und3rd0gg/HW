02410d3be2621d63720a97b5b951a56e503bc092
cfytr <5gjyjzlit@mozmail.com>
2022-03-24 16:59:07 +0300

    task 4 added

---
+++ Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task4
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string firstName = "Dylan";
+            string lastName = "Bob";
+            Console.WriteLine($"Значение переменных до перестановки: firstName = {firstName} lastName = {lastName}");
+            string buffer = lastName;
+            lastName = firstName;
+            firstName = buffer;
+            Console.WriteLine($"Значение переменных после перестановки: firstName = {firstName} lastName = {lastName}");
+        }
+    }
+}

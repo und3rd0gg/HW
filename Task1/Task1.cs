4f4b70dd6fa2489752ac52d989d76776cf1f87fa
cfytr <5gjyjzlit@mozmail.com>
2022-03-24 13:34:17 +0300

    Initial commit

---
+++ Task1/Task1.cs
@@ -0,0 +1,19 @@
+﻿namespace Homework1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int userAge = 30;
+            float averageHeight = 1.77f;
+            bool isAlive = true;
+            string name = "John";
+            char firstLetter = 'A';
+            long distance = 9341341232323;
+            uint score = 234153434;
+            double pi = 3.1415926535897932384626433;
+            uint mileage = 3000000000;
+            ushort currentLevel = 34;
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}

939e6ad60b47f3b429aa1b869a986104ce03741f
cfytr <5gjyjzlit@mozmail.com>
2022-03-25 13:51:32 +0300

    task 3 added

---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task3
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int pictureCount = 52;
+            int rowCount = 3;
+            Console.WriteLine($"Количество заполненных рядов = {pictureCount / rowCount}, количество картинок сверх меры: {pictureCount % rowCount}");
+        }
+    }
+}

9ee09191ec3c2acacfbd2a10b893bfc212fa59a6
cfytr <5gjyjzlit@mozmail.com>
2022-03-27 20:48:00 +0300

    task 5 and 6 added

---
+++ Task5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task5
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int crystalPrice = 30;
+            int goldBalance, crystalBalance = 0;
+            Console.Write("Введите начальное количество золота: ");
+            goldBalance = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите количество кристаллов для покупки: ");
+            int crystalsToBuy;
+            crystalsToBuy = Convert.ToInt32(Console.ReadLine());
+            crystalBalance += crystalsToBuy;
+            goldBalance -= crystalsToBuy * crystalPrice;
+            Console.WriteLine($"Остаток золота: {goldBalance}, остаток кристаллов: {crystalBalance}");
+        }
+    }
+}
---
+++ Task6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task6
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int grandmotherCount;
+            Console.Write("Введите количество старушек: ");
+            grandmotherCount = Convert.ToInt32(Console.ReadLine());
+            int appointmentTime = 10;
+            int timeToWait = grandmotherCount * appointmentTime;
+            Console.Write("Вы должны отстоять в очереди ");
+            int hour = 60;
+            if (timeToWait > hour)
+            {
+                int hoursToWait = timeToWait / hour;
+                Console.Write($"{hoursToWait} часа ");
+            }
+
+            int minutesToWait = timeToWait % hour;
+            Console.Write($"{minutesToWait} минут.");
+        }
+    }
+}

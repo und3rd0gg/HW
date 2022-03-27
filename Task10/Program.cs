b88b14c5f61305945b5e9d9797633b015eac8473
cfytr <5gjyjzlit@mozmail.com>
2022-03-28 01:19:37 +0300

    t10 added

---
+++ Task10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task10
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Random random = new Random();
+            int number = random.Next(0, 101);
+            int summ = 0;
+            Console.WriteLine(number);
+            
+            for (; number > 0; number--)
+            {
+                
+                if (number % 3 == 0 || number % 5 == 0)
+                {
+                    summ += number;
+                }
+                
+            }
+            
+            Console.WriteLine($"Полученная сумма = {summ}");
+        }
+    }
+}
---
+++ Task9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task9
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int startNumber = 5;
+            int endNumber = 96;
+            int difference = 7;
+
+            for (int increasingNumber = startNumber; increasingNumber <= endNumber; increasingNumber += difference)
+            {
+                Console.WriteLine(increasingNumber);
+            }
+        }
+    }
+}

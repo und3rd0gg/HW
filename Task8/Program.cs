54c59b93f52299aec41c1497a9a63580078b6ed0
cfytr <5gjyjzlit@mozmail.com>
2022-03-28 00:39:56 +0300

    t8 added

---
+++ Task8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task8
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            ulong number = 14;
+            string userInput = "";
+
+            while (userInput != "exit")
+            {
+                Console.WriteLine($"Число: {number}");
+                number *= number;
+                Console.WriteLine("Введите любой символ, чтобы возвести число в квадрат или ");
+                Console.WriteLine("введите exit чтобы завершить программу.");
+                userInput = Console.ReadLine();
+            }
+        }
+    }
+}

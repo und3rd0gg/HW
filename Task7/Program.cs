8d80e9d09488cec18bed13d6c81571e25634c5c1
cfytr <5gjyjzlit@mozmail.com>
2022-03-27 21:07:27 +0300

    task 7 added

---
+++ Task7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task7
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string message;
+            Console.Write("Введите сообщение: ");
+            message = Console.ReadLine();
+            int repeatCount;
+            Console.Write("Введите количество повторов: ");
+            repeatCount = Convert.ToInt32(Console.ReadLine());
+            while (repeatCount > 0)
+            {
+                Console.WriteLine(message);
+                repeatCount--;
+            }
+        }
+    }
+}

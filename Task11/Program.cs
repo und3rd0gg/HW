213feb1aafd9e01a571b90006b6943ae120fdf97
cfytr <5gjyjzlit@mozmail.com>
2022-03-28 17:03:34 +0300

    t 11 12 added

---
+++ Task11/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Task11
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            float usdBalance = 130f;
+            float eurBalance = 320f;
+            float cnyBalance = 2440f;
+            float usdEurRate = 0.91f;
+            float usdCnyRate = 6.37f;
+            float eurUsdRate = 1.09f;
+            float eurCnyRate = 6.97f;
+            float cnyUsdRate = 0.16f;
+            float cnyEurRate = 0.14f;
+            string userInput = "";
+
+            while (userInput != "exit")
+            {
+                Console.WriteLine($"Ваш баланс: \nUSD: {usdBalance}\nEUR: {eurBalance}\nCNY: {cnyBalance}");
+                Console.WriteLine("Введите действие: ");
+                Console.WriteLine("1. USD -> EUR");
+                Console.WriteLine("2. USD -> CNY");
+                Console.WriteLine("3. EUR -> USD");
+                Console.WriteLine("4. EUR -> CNY");
+                Console.WriteLine("5. CNY -> USD");
+                Console.WriteLine("6. CNY -> EUR");
+                Console.WriteLine("Введите exit чтобы завершить программу.");
+                userInput = Console.ReadLine();
+                int selectedAction = 0;
+                
+                if (userInput != "exit")
+                {
+                    selectedAction = Convert.ToInt32(userInput);
+                }
+                
+                Console.WriteLine("Введите количество валюты для конвертации");
+                int amountToConvert = Convert.ToInt32(Console.ReadLine());
+
+                switch (selectedAction)
+                {
+                    case 1:
+                        
+                        usdBalance -= amountToConvert;
+                        eurBalance += amountToConvert * usdEurRate;
+                        break;
+                    
+                    case 2:
+                        
+                        usdBalance -= amountToConvert;
+                        cnyBalance += amountToConvert * cnyUsdRate;
+                        break;
+                    
+                    case 3:
+
+                        eurBalance -= amountToConvert;
+                        usdBalance += amountToConvert * eurUsdRate;
+                        break;
+                    
+                    case 4:
+
+                        eurBalance -= amountToConvert;
+                        cnyBalance += amountToConvert * eurCnyRate;
+                        break;
+                    
+                    case 5:
+
+                        cnyBalance -= amountToConvert;
+                        usdBalance += amountToConvert * cnyUsdRate;
+                        break;
+                    
+                    case 6:
+
+                        cnyBalance -= amountToConvert;
+                        eurBalance += amountToConvert * cnyEurRate;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Task12/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace Task12
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string name = "";
+            string password = "";
+            string userInput = "";
+
+            while (userInput != "Esc")
+            {
+                Console.WriteLine("Введите команду");
+                Console.WriteLine("SetName – установить имя\nChangeConsoleColor- изменить цвет консоли" +
+                                  "\nSetPassword – установить пароль\nWriteName – вывести имя\n" +
+                                  "DeletePassword - Удалить пароль\nEsc – выход из программы");
+                userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "SetName":
+
+                        if (name != "")
+                            Console.WriteLine("Имя уже существует, если вы продолжите, оно будет перезаписано");
+
+                        Console.WriteLine("Введите желаемое имя");
+                        name = Console.ReadLine();
+                        break;
+
+                    case "ChangeConsoleColor":
+                        Console.WriteLine(
+                            "Введите желаемый цвет консоли: \nGreen - Зеленый\nWhite - Белый\nBlack - Черный\nRed - Красный");
+                        string selectedColor = Console.ReadLine();
+
+                        switch (selectedColor)
+                        {
+                            case "Green":
+
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.BackgroundColor = ConsoleColor.Black;
+                                Console.Clear();
+                                break;
+
+                            case "White":
+
+                                Console.ForegroundColor = ConsoleColor.Black;
+                                Console.BackgroundColor = ConsoleColor.White;
+                                Console.Clear();
+                                break;
+
+                            case "Black":
+
+                                Console.ForegroundColor = ConsoleColor.White;
+                                Console.BackgroundColor = ConsoleColor.Black;
+                                Console.Clear();
+                                break;
+
+                            case "Red":
+
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.BackgroundColor = ConsoleColor.DarkRed;
+                                break;
+                        }
+
+                        break;
+
+                    case "SetPassword":
+
+                        if (password != "")
+                        {
+                            Console.WriteLine("Введите старый пароль:");
+                            string inputPassword = Console.ReadLine();
+
+                            if (inputPassword != password)
+                            {
+                                Console.WriteLine("Введен неправильный пароль\n");
+                                continue;
+                            }
+                        }
+
+                        Console.WriteLine("Введите новый пароль");
+                        password = Console.ReadLine();
+                        Console.WriteLine("Пароль изменен\n");
+                        break;
+
+                    case "WriteName":
+
+                        if (password != "")
+                        {
+                            Console.WriteLine("Введите пароль");
+                            string inputPassword = Console.ReadLine();
+
+                            if (inputPassword != password)
+                            {
+                                Console.WriteLine("Пароль введен неправильно\n");
+                                continue;
+                            }
+                        }
+
+                        Console.WriteLine($"Ваше имя: {name}");
+                        break;
+
+                    case "DeletePassword":
+
+                        if (password != "")
+                        {
+                            Console.WriteLine("Введите пароль");
+                            string inputPassword = Console.ReadLine();
+
+                            if (inputPassword != password)
+                            {
+                                Console.WriteLine("Пароль введен неправильно");
+                                continue;
+                            }
+                        }
+
+                        Console.WriteLine("Введите новый пароль");
+                        password = Console.ReadLine();
+                        Console.WriteLine("Пароль успешно изменен\n");
+                        break;
+                }
+            }
+        }
+    }
+}

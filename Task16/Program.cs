e4a689e3efb6f6fdf47c5cf4a1498a04d861ac75
cfytr <5gjyjzlit@mozmail.com>
2022-03-29 16:07:26 +0300

    16 17 added

---
+++ Task16/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task16
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Random random = new Random();
+            int number = random.Next(0, 1001);
+            double two = 2;
+            double twoInPower = 0;
+            double power = 0;
+
+            while (twoInPower < number)
+            {
+                power++;
+                twoInPower = Math.Pow(two, power);
+            }
+            
+            Console.WriteLine($"Число: {number}, Степень: {power}");
+        }
+    }
+}
---
+++ Task17/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Task17
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string stringToCheck = "(()(()(())))";
+            int openBracketsCount = 0;
+            int closingBracketsCount = 0;
+            int depth = 0;
+            int maxDepth = 0;
+
+            foreach (var symbol in stringToCheck)
+            {
+
+                if (symbol == '(')
+                {
+                    openBracketsCount++;
+                    depth++;
+                }
+                else if (symbol == ')')
+                {
+                    closingBracketsCount++;
+                    depth--;
+                }
+
+                if (maxDepth < depth)
+                {
+                    maxDepth = depth;
+                }
+            }
+
+            if (openBracketsCount != closingBracketsCount)
+            {
+                Console.WriteLine("Некорректное скобочное выражение");
+            }
+            else
+            {
+                Console.WriteLine($"Корректное скобочное выражение, максимальная глубина вложенности: {maxDepth}");
+            }
+        }
+    }
+}

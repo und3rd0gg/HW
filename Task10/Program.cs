5c0585398323512726a7b010988de95abf176e33
cfytr <5gjyjzlit@mozmail.com>
2022-03-29 14:00:53 +0300

    fix

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -10,14 +10,14 @@ namespace Task10
             int randomStartNumber = 0;
             int randomEndNumber = 101;
             int number = random.Next(randomStartNumber, randomEndNumber);
-            int firstNumberToCheck = 3;
-            int secondNumberToCheck = 5;
+            int divisorByThree = 3;
+            int divisorByFive = 5;
             int summ = 0;
 
             for (; number > 0; number--)
             {
-                bool isNumberMultiplesBy3 = number % firstNumberToCheck == 0;
-                bool isNumberMultiplesBy5 = number % secondNumberToCheck == 0;
+                bool isNumberMultiplesBy3 = number % divisorByThree == 0;
+                bool isNumberMultiplesBy5 = number % divisorByFive == 0;
                 
                 if (isNumberMultiplesBy3 || isNumberMultiplesBy5)
                 {

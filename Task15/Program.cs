989e82b36d83b065a8061c325abf7fa183e83ea3
cfytr <5gjyjzlit@mozmail.com>
2022-03-30 22:12:46 +0300

    t15 added

---
+++ Task15/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task15
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomStartValue = 1;
+            int randomEndValue = 28;
+            int N = random.Next(randomStartValue, randomEndValue);
+            int firstSearchLowerLimit = 100;
+            int secondSearchLowerLimit = 0;
+            int searchUpperLimit = 999;
+            int foundMultiplesCount = 0;
+            
+            while (secondSearchLowerLimit < firstSearchLowerLimit)
+            {
+                secondSearchLowerLimit += N;
+            }
+
+            for (int i = secondSearchLowerLimit; i <= searchUpperLimit; i += N)
+            {
+                foundMultiplesCount++;
+            }
+
+            Console.WriteLine($"Число N: {N}, Количество трехзначных кратных чисел: {foundMultiplesCount}");
+        }
+    }
+}
